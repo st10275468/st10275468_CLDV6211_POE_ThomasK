c5741991dafff3e51044ba6bcbe1e9d0371651a9
st10275468 <st10275468@vcconnect.edu.za>
2024-05-17 12:40:27 +0200

    TransactionController completed

--- st10275468_CLDV6211_POE_ThomasK/Controllers/TransactionController.cs
+++ st10275468_CLDV6211_POE_ThomasK/Controllers/TransactionController.cs
@@ -1,12 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using st10275468_CLDV6211_POE_ThomasK.Models;
+using System.Data.SqlClient;
+using System.Linq.Expressions;
 
 namespace st10275468_CLDV6211_POE_ThomasK.Controllers
 {
     public class TransactionController : Controller
     {
-        public IActionResult Index()
+        [HttpPost]
+        public ActionResult PlaceOrder(int userID, int productID)
         {
-            return View();
+            try
+            {
+                using (SqlConnection con = new SqlConnection(Products.con_string))
+                {
+
+                    string sql = "INSERT INTO Transactions (UserID, ProductID) VALUES (@UserID, @ProductID)";
+
+                    using (SqlCommand cmd = new SqlCommand(sql, con))
+                    {
+                        cmd.Parameters.AddWithValue("@UserID", userID);
+                        cmd.Parameters.AddWithValue("@ProductID", productID);
+
+                        con.Open();
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        con.Close();
+
+                        if (rowsAffected > 0)
+                        {
+                            return RedirectToAction("Index", "Home");
+                        }
+                        else
+                        {
+                            return View("OrderFailed");
+                        }
+                    }
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                return View("Error");
+            }
         }
     }
 }
