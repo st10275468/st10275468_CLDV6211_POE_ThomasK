780ffb2a07f13701065be93b8c2c13a71e5744ce
st10275468 <st10275468@vcconnect.edu.za>
2024-05-17 12:25:28 +0200

    DisplayProductsController completed

--- st10275468_CLDV6211_POE_ThomasK/Controllers/DisplayProductsController.cs
+++ st10275468_CLDV6211_POE_ThomasK/Controllers/DisplayProductsController.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using st10275468_CLDV6211_POE_ThomasK.Models;
 
 namespace st10275468_CLDV6211_POE_ThomasK.Controllers
 {
     public class DisplayProductsController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            var Products = DisplayProducts.SelectProducts();
+            return View(Products);
         }
     }
 }
