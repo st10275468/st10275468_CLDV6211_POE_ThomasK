bea4329d054d2a1c532c0d9ae53307a6ff53eb6b
st10275468 <st10275468@vcconnect.edu.za>
2024-05-17 12:44:55 +0200

    UserController completed

--- st10275468_CLDV6211_POE_ThomasK/Controllers/UserController.cs
+++ st10275468_CLDV6211_POE_ThomasK/Controllers/UserController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using st10275468_CLDV6211_POE_ThomasK.Models;
 
 namespace st10275468_CLDV6211_POE_ThomasK.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult Index()
+       public Users userTbl = new Users();
+
+        [HttpPost]
+        public ActionResult About(Users users)
         {
-            return View();
+            var result = userTbl.insert_User(users);
+            return RedirectToAction("Privacy", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult About()
+        {
+            return View(userTbl);
         }
     }
 }
