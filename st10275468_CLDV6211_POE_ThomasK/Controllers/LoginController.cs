0cb15866ffd253f125b21aa177b63839237366ae
st10275468 <st10275468@vcconnect.edu.za>
2024-05-17 12:18:25 +0200

    LoginController completed

--- st10275468_CLDV6211_POE_ThomasK/Controllers/LoginController.cs
+++ st10275468_CLDV6211_POE_ThomasK/Controllers/LoginController.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using st10275468_CLDV6211_POE_ThomasK.Models;
 
 namespace st10275468_CLDV6211_POE_ThomasK.Controllers
 {
     public class LoginController : Controller
     {
-        public IActionResult Index()
+        private readonly Login loginM;
+
+        public LoginController()
         {
-            return View();
+            loginM = new Login();
         }
+
+        [HttpPost]
+
+        public ActionResult Privacy(string password, string name)
+        {
+            var loginModel = new Login();
+            int userID = loginModel.SelectUser(password, name);
+            if (userID != -1)
+            {
+
+                HttpContext.Session.SetInt32("userID", userID);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View("LoginFailed");
+            }
+        }
+
+        
     }
 }
